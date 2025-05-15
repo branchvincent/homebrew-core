0009002591bfd73bfaaeac434eba6303f5ed1020
Branch Vincent <branchevincent@gmail.com>
2025-05-14 19:07:15 -0700

    redex 2025.03.31

--- Formula/r/redex.rb
+++ Formula/r/redex.rb
@@ -4,39 +4,10 @@ class Redex < Formula
 
   desc "Bytecode optimizer for Android apps"
   homepage "https://fbredex.com/"
+  url "https://github.com/facebook/redex/archive/refs/tags/v2025.03.31.tar.gz"
+  sha256 "39f43b887bd55b1910de65159960da8ff126d4918c2a3c785e70ddc5ffec148d"
   license "MIT"
-  revision 19
   head "https://github.com/facebook/redex.git", branch: "main"
-
-  stable do
-    url "https://github.com/facebook/redex/archive/refs/tags/v2017.10.31.tar.gz"
-    sha256 "18a840e4db0fc51f79e17dfd749b2ffcce65a28e7ef9c2b3c255c5ad89f6fd6f"
-
-    # Fix for automake 1.16.5
-    patch do
-      url "https://github.com/facebook/redex/commit/4696e1882cf88707bf7560a2994a4207a8b7c7a3.patch?full_index=1"
-      sha256 "dccc41146688448ea2d99dd04d4d41fdaf7e174ae1888d3abb10eb2dfa6ed1da"
-    end
-
-    # Apply upstream fixes for GCC 11
-    patch do
-      url "https://github.com/facebook/redex/commit/70a82b873da269e7dd46611c73cfcdf7f84efa1a.patch?full_index=1"
-      sha256 "44ce35ca93922f59fb4d0fd1885d24cce8a08d73b509e1fd2675557948464f1d"
-    end
-    patch do
-      url "https://github.com/facebook/redex/commit/e81dda3f26144a9c94816c12237698ef2addf864.patch?full_index=1"
-      sha256 "523ad3d7841a6716ac973b467be3ea8b6b7e332089f23e4788e1f679fd6f53f5"
-    end
-    patch do
-      url "https://github.com/facebook/redex/commit/253b77159d6783786c8814168d1ff2b783d3a531.patch?full_index=1"
-      sha256 "ed69a6230506704ca4cc7a52418b3af70a6182bd96abdb5874fab02f6b1a7c99"
-    end
-
-    # Fix compilation on High Sierra
-    # Fix boost issue (https://github.com/facebook/redex/pull/564)
-    # Remove for next release
-    patch :DATA
-  end
 
   bottle do
     sha256 cellar: :any,                 arm64_sequoia: "65347da8f446206921fbb0603b12153dacc92e1eaa161b1b74ebb38576d5ac3c"
@@ -56,37 +27,26 @@ class Redex < Formula
   depends_on "jsoncpp"
   depends_on "python@3.13"
 
-  resource "setuptools" do
-    url "https://files.pythonhosted.org/packages/4d/5b/dc575711b6b8f2f866131a40d053e30e962e633b332acf7cd2c24843d83d/setuptools-69.2.0.tar.gz"
-    sha256 "0ff4183f8f42cd8fa3acea16c45205521a4ef28f73c6391d8a25e92893134f2e"
+  resource "packaging" do
+    url "https://files.pythonhosted.org/packages/a1/d4/1fc4078c65507b51b96ca8f8c3ba19e6a61c8253c72794544580a7b6c24d/packaging-25.0.tar.gz"
+    sha256 "d443872c98d677bf60f6a1f2f8c1cb748e8fe762d2bf9d3148b5599295b0fc4f"
+  end
+
+  # Replace `distutils` with `packaging` for python 3.12+
+  patch do
+    url "https://github.com/facebook/redex/commit/aecf5d7e595ed1ece7cabc16e974280b97ca7efc.patch?full_index=1"
+    sha256 "3ae2e72634d33bb3d2b7668b7ce864adbbcca053f15b83b383ec505e304e8553"
   end
 
   def install
-    if build.stable?
-      # https://github.com/facebook/redex/issues/457
-      inreplace "Makefile.am", "/usr/include/jsoncpp", Formula["jsoncpp"].opt_include
-      # Work around missing include. Fixed upstream but code has been refactored
-      # Ref: https://github.com/facebook/redex/commit/3f4cde379da4657068a0dbe85c03df558854c31c
-      ENV.append "CXXFLAGS", "-include set"
-      # Help detect Boost::Filesystem and Boost::System during ./configure.
-      # TODO: Remove in the next release.
-      ENV.cxx11
-    end
+    # Skip tests, which require an Android SDK
+    inreplace "Makefile.am", "SUBDIRS = . test", "SUBDIRS = ."
+    # Replace `pipes` usage for python 3.13
+    inreplace "redex.py", "from pipes import quote", "from shlex import quote"
 
     venv = virtualenv_create(libexec, "python3.13")
     venv.pip_install resources
-
-    python_scripts = %w[
-      apkutil
-      redex.py
-      tools/python/dex.py
-      tools/python/dict_utils.py
-      tools/python/file_extract.py
-      tools/python/reach_graph.py
-      tools/redex-tool/DexSqlQuery.py
-      tools/redexdump-apk
-    ]
-    rewrite_shebang python_shebang_rewrite_info(venv.root/"bin/python"), *python_scripts
+    rewrite_shebang python_shebang_rewrite_info(venv.root/"bin/python"), "redex.py"
 
     system "autoreconf", "--force", "--install", "--verbose"
     system "./configure", "--disable-silent-rules",
@@ -103,40 +63,9 @@ class Redex < Formula
     end
 
     testpath.install resource("homebrew-test_apk")
-    system bin/"redex", "--ignore-zipalign", "redex-test.apk", "-o", "redex-test-out.apk"
+    system bin/"redex", "--ignore-zipalign",
+                        "-Jignore_no_keep_rules=true",
+                        "redex-test.apk", "-o", "redex-test-out.apk"
     assert_path_exists testpath/"redex-test-out.apk"
   end
 end
-
-__END__
-diff --git a/libresource/RedexResources.cpp b/libresource/RedexResources.cpp
-index 525601ec..a359f49f 100644
---- a/libresource/RedexResources.cpp
-+++ b/libresource/RedexResources.cpp
-@@ -16,6 +16,7 @@
- #include <map>
- #include <boost/regex.hpp>
- #include <sstream>
-+#include <stack>
- #include <string>
- #include <unordered_set>
- #include <vector>
-diff --git a/libredex/Show.cpp b/libredex/Show.cpp
-index b042070f..5e492e3f 100644
---- a/libredex/Show.cpp
-+++ b/libredex/Show.cpp
-@@ -9,7 +9,14 @@
-
- #include "Show.h"
-
-+#include <boost/version.hpp>
-+// Quoted was accepted into public components as of 1.73. The `detail`
-+// header was removed in 1.74.
-+#if BOOST_VERSION < 107400
- #include <boost/io/detail/quoted_manip.hpp>
-+#else
-+#include <boost/io/quoted.hpp>
-+#endif
- #include <sstream>
-
- #include "ControlFlow.h"
